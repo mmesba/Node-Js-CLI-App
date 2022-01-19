bcb8c4d061b57969130e7595ef03865fbb6b2406
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-01-20 01:13:27 +0600

    index file initiated

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+ * Title: Primary File for the CLI App
+ * Description: Index or Primary file for cli app
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 20/01/2022
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
+ 
