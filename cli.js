5e29b25e3130e160e6671cd3ee4d22de027c8d22
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-01-20 01:54:40 +0600

    cli file ininitiated

---
+++ cli.js
@@ -0,0 +1,22 @@
+/*
+ * Title: CLI file
+ * Description: CLI related function to build cli app
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mmesba
+ * Date: 20/01/2022
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+const cli = {} 
+// main functions or objects.
+ 
+ 
+ cli.test = ()=>{
+     console.log('\x1b[34m%s\x1b','ok');
+ }
+ 
+// export the module.
+ module.exports = cli;
