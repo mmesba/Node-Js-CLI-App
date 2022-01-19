d66a7903dbf59b19470f0e2829c896d5046fd6d1
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2022-01-20 01:32:46 +0600

    Basic server created

--- index.js
+++ index.js
@@ -7,14 +7,42 @@
  */
  
 // Dependencies.
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
+
  
  
 // App object or Module scaffolding.
- 
+const server = {}; 
 // main functions or objects.
+//  Creating http server
+server.createHttpServer = ()=>{
+    const createHttpServerVariable = http.createServer(server.handleReqRes);
+    createHttpServerVariable.listen(3000, ()=>{
+        console.log('\x1b[33m%s\x1b' ,`Listening on port 3000`);
+    })
+}
  
+// Defining handle req res function
+ server.handleReqRes = (req, res)=>{
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+
+    res.setHeader('Content-Type', 'Application/json')
+    // res.writeHead(statusCode)
+    res.write('ok')
+    console.log('\x1b[34m%s\x1b', 'pinging...');
+    res.end();
+ }
  
- 
- 
+ server.createHttpServer();
 // export the module.
- 
+ module.exports = server;
